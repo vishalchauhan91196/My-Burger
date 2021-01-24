75172d28f38f95361acf329ec0c80e5a557aa83f
vishalchauhan91196 <vishalchauhan91196@gmail.com>
2021-01-24 10:07:56 +0530

    Initial Commit

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className= {classes.NavigationItems} >
-        <NavigationItem link= '/'  >Burger Builder </NavigationItem> 
+        <NavigationItem link= '/'  > My Burger </NavigationItem> 
     
         {props.isAuthenticated ? <NavigationItem link= '/order' > Orders </NavigationItem> : null }
     
